d6cdb03cbc87d3c0934d6096be81066c8270133a
Jacob <jacob.d.wencloff@gmail.com>
2022-06-14 11:21:31 -0700

    Moved the navigation droppdown from the Home.js component into App.js to maintaine the Nav between various components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,75 @@
 import logo from './logo.svg';
-import {Link, Routes, Route} from "react-router-dom"
+import { Link, Routes, Route } from "react-router-dom"
+import { Container, Row, Col, Button, Offcanvas, OffcanvasBody } from 'reactstrap'
+import {useState} from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 import Home from './Components/Home';
 import Skills from './Components/Skills';
-
+import Projects from './Components/Projects'
+import ContactMe from './Components/ContactME'
 
 function App() {
+  const [isActive, setIsActive] = useState(false)
+  const [hamburger, setHamburger] = useState('|||')
+
+  const handleNav = (e) =>{
+    if(isActive === false){
+        setIsActive(true)
+        setHamburger('X')
+    }else{
+        setIsActive(false)
+        setHamburger('|||')
+    }
+}
+
   return (
     <div className="App">
+      <Offcanvas style={{ height: "100px" }} direction='top' isOpen={isActive} onClick={handleNav}>
+        <OffcanvasBody style={{ opacity: "1" }}>
+          <Container>
+            <Row>
+              <Col>
+                <Link to="/Skills">
+                  <Button style={{ opacity: "1" }}>Skills</Button>
+                </Link>
+              </Col>
+              <Col>
+                <Link to="/Projects">
+                  <Button>Projects</Button>
+                </Link>
+              </Col>
+              <Col>
+                <Link to="/ContactMe">
+                  <Button>Contact Me</Button>
+                </Link>
+              </Col>
+              <Col>
+              <Link to="/">
+                <Button>Home</Button>
+              </Link>
+              </Col>
+            </Row>
+          </Container>
+        </OffcanvasBody>
+      </Offcanvas>
+      <Container>
+        <Row>
+          <Col sm="11">
+          </Col>
+          <Col sm="1">
+            <Button onClick={handleNav}>{hamburger}</Button>
+          </Col>
+        </Row>
+      </Container>
       <Link to='/' />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/Skills' element={<Skills/>} />
+        <Route path='/Skills' element={<Skills />} />
+        <Route path='/Projects' element={<Projects />} />
+        <Route path='/ContactMe' element={<ContactMe />} />
       </Routes>
-      
+
     </div>
   );
 }
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,52 +1,15 @@
 
 import React from 'react'
-import { Link } from 'react-router-dom'
 import { Container, Row, Col, Button } from 'reactstrap'
-import {useState} from 'react'
-import { Offcanvas } from 'reactstrap'
-import { OffcanvasBody } from 'reactstrap'
+
 
 export default function Home() {
-    const [isActive, setIsActive] = useState(false)
-    const [hamburger, setHamburger] = useState('|||')
-
-    const handleNav = (e) =>{
-        if(isActive === false){
-            setIsActive(true)
-            setHamburger('X')
-        }else{
-            setIsActive(false)
-            setHamburger('|||')
-        }
-    }
     return (
         <div>
-            <Offcanvas style={{height: "100px"}} direction='top' isOpen={isActive} onClick={handleNav}>
-                <OffcanvasBody style={{opacity: "1"}}>
-                    <Container>
-                        <Row>
-                            <Col>
-                            <Link to="/Skills">
-                                <Button style={{opacity: "1"}}>Skills</Button>
-                            </Link>
-                            </Col>
-                            <Col>
-                                <Button>Projects</Button>
-                            </Col>
-                            <Col>
-                                <Button>Contact Me</Button>
-                            </Col>
-                        </Row>
-                    </Container>
-                </OffcanvasBody>
-            </Offcanvas>
             <Container>
                 <Row>
                     <Col sm="11">
                         <h1>Jacob Wencloff</h1>
-                    </Col>
-                    <Col sm="1"> 
-                        <Button onClick={handleNav}>{hamburger}</Button>
                     </Col>
                 </Row>
                 <Row>
