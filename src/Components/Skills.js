ced03893863a1a10fb321c921033237650754304
Jacob <jacob.d.wencloff@gmail.com>
2022-06-14 13:02:54 -0700

    Added content to the Skills page including some basic skills badges and a breif overveiw of my skill suit

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -1,7 +1,81 @@
 import React from 'react'
+import { Container, Row, Col, Card, CardBody, CardImg } from 'reactstrap'
 
 export default function Skills() {
   return (
-    <div>Skills</div>
+    <div>
+      <Container>
+        <Row>
+          <Col sm="11">
+            <h1>Skill's</h1>
+          </Col>
+        </Row>
+        <Row>
+          <p>Through my education at General Assembly, along with self taugh practices, i've learned several computer
+            languages and supporting libraries along with the ability to inpliment CRUD and a RESTful API interface!
+            Below is a list of all of my current skills and practices </p>
+        </Row>
+        <Row>
+          <Col>
+            <Card>
+              <CardBody>
+                <CardImg alt="HTML5 Badge" src='./HTML5_Logo_128.png' />
+              </CardBody>
+            </Card>
+          </Col>
+          <Col>
+            <Card>
+              <CardBody>
+              <CardImg alt="CSS3 Badge" src='./css.png' />
+              </CardBody>
+            </Card>
+          </Col>
+          <Col>
+            <Card>
+              <CardBody>
+
+              </CardBody>
+            </Card>
+          </Col>
+          <Col>
+            <Card>
+              <CardBody>
+
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+        <Row style={{marginTop: "30px"}}>
+          <Col>
+            <Card>
+              <CardBody>
+                
+              </CardBody>
+            </Card>
+          </Col>
+          <Col>
+            <Card>
+              <CardBody>
+
+              </CardBody>
+            </Card>
+          </Col>
+          <Col>
+            <Card>
+              <CardBody>
+
+              </CardBody>
+            </Card>
+          </Col>
+          <Col>
+            <Card>
+              <CardBody>
+
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   )
 }
